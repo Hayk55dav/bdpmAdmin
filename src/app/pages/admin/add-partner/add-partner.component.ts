08eb0d3828787c3d41086a06a847ae444f1e806b
unknown <hayk9710@gmail.com>
2018-04-13 17:27:57 +0400

    'add'

--- src/app/pages/admin/add-partner/add-partner.component.ts
+++ src/app/pages/admin/add-partner/add-partner.component.ts
@@ -32,6 +32,16 @@ export class AddPartnerComponent implements OnInit {
     emailFormControl;
     matcher;
     errorMessage: string;
+    userNameFormControl;
+    phoneFormControl;
+    companyNameFormControl;
+    stateFormControl;
+    cityFormControl;
+    addressFormControl;
+    zipCodeFormControl;
+    faxFormControl;
+    incomePercentFormControl;
+    debitFormControl;
     constructor(private errorStateMatcher: ErrorStateMatcher, private dataService: DataService, public dialogRef: MatDialogRef<PartnersComponent>,
                 @Inject(MAT_DIALOG_DATA) public data: any) {
     }
@@ -39,9 +49,42 @@ export class AddPartnerComponent implements OnInit {
     ngOnInit() {
         this.emailFormControl = new FormControl('', [
             Validators.required,
-            Validators.email,
+            Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/)
         ]);
-
+        this.userNameFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/)
+        ]);
+        this.phoneFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
+        ]);
+        this.companyNameFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.stateFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^[a-zA-Z\s]*$/),
+        ]);
+        this.cityFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^[a-zA-Z\s]*$/),
+        ]);
+        this.addressFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.zipCodeFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.faxFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.incomePercentFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.debitFormControl = new FormControl('', [
+            Validators.required,
+        ]);
         this.matcher = this.errorStateMatcher;
     }
 
--- src/app/pages/admin/edit-partner/edit-partner.component.ts
+++ src/app/pages/admin/edit-partner/edit-partner.component.ts
@@ -19,6 +19,16 @@ export class EditPartnerComponent implements OnInit {
     matcher;
     active;
     errorMessage: string;
+    userNameFormControl;
+    phoneFormControl;
+    companyNameFormControl;
+    stateFormControl;
+    cityFormControl;
+    addressFormControl;
+    zipCodeFormControl;
+    faxFormControl;
+    incomePercentFormControl;
+    debitFormControl;
     constructor(private errorStateMatcher: ErrorStateMatcher, private  dataService: DataService, public dialogRef: MatDialogRef<PartnersComponent>,
                 @Inject(MAT_DIALOG_DATA) public data: any) {
 
@@ -34,10 +44,44 @@ export class EditPartnerComponent implements OnInit {
         }
         this.emailFormControl = new FormControl('', [
             Validators.required,
-            Validators.email,
+            Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,5}))$/)
         ]);
-
+        this.userNameFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?=.{5,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/)
+        ]);
+        this.phoneFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/)
+        ]);
+        this.companyNameFormControl = new FormControl('', [
+            Validators.required,
+            ]);
+        this.stateFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^[a-zA-Z\s]*$/),
+        ]);
+        this.cityFormControl = new FormControl('', [
+            Validators.required,
+            Validators.pattern(/^[a-zA-Z\s]*$/),
+        ]);
+        this.addressFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.zipCodeFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.faxFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.incomePercentFormControl = new FormControl('', [
+            Validators.required,
+        ]);
+        this.debitFormControl = new FormControl('', [
+            Validators.required,
+        ]);
         this.matcher = this.errorStateMatcher;
+        console.log(this.matcher);
     }
 
     saveEdit(){
